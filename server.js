d9f12b53dfad39e2fd7746092a10ddb63aa77c6e
Jim Lind <spoon.vw@gmail.com>
2023-02-18 22:50:32 -0500

    Add Secret Logging

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ http.createServer((request, response) => {
 }).listen(process.env.PORT || 8080);
 
 console.log(process.env);
+
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager").v1;
+const secretManagerClient = new SecretManagerServiceClient({
+    keyFilename: "",
+});
+
+const name = `projects/letterboxd-bot/secrets/DISCORD_DEV_BOT_TOKEN/versions/latest`;
+secretManagerClient
+    .accessSecretVersion({ name })
+    .then(([response]) => console.log(response.payload.data.toString("utf8")));
